2212b03df87379ce57c226698282119aada36fac
kenzoutenma <pankakiril@gmail.com>
2025-10-26 13:09:22 +0200

    refactored: anime list loading behavior

--- src/app/(anime)/list/page.tsx
+++ src/app/(anime)/list/page.tsx
@@ -1,9 +1,8 @@
 import Pagination from '@/components/Pagination/Pagination';
-import { Button, Section } from '@/components/UI/UIComponents';
+import { Button } from '@/components/UI/UIComponents';
 import useAnimeList from '@/hooks/useAnimeList';
 import { Metadata } from 'next';
 import AnimeList from './Components/AnimeList';
-import Filters from './Components/Filters';
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
@@ -21,26 +20,21 @@ const listPage = async ({ searchParams }: ListPageProps) => {
   const AnimeData = await useAnimeList(filterQuery);
 
   return (
-    <Section typeOfSection="TwoColSection" classname="justify-evenly" style={{ gridTemplateColumns: "10fr 3fr", padding: "2rem 6rem"}}>
-      <Section.Col>
-        <AnimeList anime={AnimeData.titles} />
-        <Pagination isNextDisabled={!AnimeData.isNextPage} isPrevDisabled={!AnimeData.isPrevPage}>
-          {Array.from({ length: AnimeData.pageCount }).map((_, i) => (
-            <Button
-              as="a"
-              href={AnimeData.createPageUrl(i + 1)}
-              key={i}
-              variant={AnimeData.page == i + 1 ? 'primary' : 'secondary'}
-            >
-              {i + 1}
-            </Button>
-          ))}
-        </Pagination>
-      </Section.Col>
-      <Section.Col>
-        <Filters />
-      </Section.Col>
-    </Section>
+    <>
+      <AnimeList anime={AnimeData.titles} />
+      <Pagination isNextDisabled={!AnimeData.isNextPage} isPrevDisabled={!AnimeData.isPrevPage}>
+        {Array.from({ length: AnimeData.pageCount }).map((_, i) => (
+          <Button
+            as="a"
+            href={AnimeData.createPageUrl(i + 1)}
+            key={i}
+            variant={AnimeData.page == i + 1 ? 'primary' : 'secondary'}
+          >
+            {i + 1}
+          </Button>
+        ))}
+      </Pagination>
+    </>
   );
 };
 
