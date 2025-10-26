ad3bc3379ce540f59015a485e419743fd1ef1ac4
kenzoutenma <pankakiril@gmail.com>
2025-10-26 13:27:31 +0200

    fix: anime page layouting

--- src/components/Screens/Anime/Sections/PlayerSection/PlayerSection.tsx
+++ src/components/Screens/Anime/Sections/PlayerSection/PlayerSection.tsx
@@ -1,22 +1,16 @@
 'use client';
 
+import { Section } from '@/components/UI/UIComponents';
 import Image from 'next/image';
 import Player from '../../Player/player';
 import AddToCollectionButton from '../../addToCollectionButton';
 import styles from './PlayerSection.module.css';
-import { Section } from '@/components/UI/UIComponents';
 
 function PlayerSection({ data }: { data: AnimeDataInterface }) {
   return (
     <Section typeOfSection="TwoColSection" style={{ gridTemplateColumns: '3fr 7fr' }}>
       <div className={styles.playerSectionColumn}>
-        <Image
-          src={data.poster}
-          alt={data.title}
-          height={350}
-          width={250}
-          style={{ width: '100%', borderRadius: 'var(--borderRad)' }}
-        />
+        <Image src={data.poster} alt={data.title} height={350} width={250} style={{}} />
         <AddToCollectionButton slug={data.slug} />
       </div>
       <div className={styles.playerSectionColumn}>
