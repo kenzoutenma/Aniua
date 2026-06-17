6d04da4c040cf7987b43517a5c96fff6a714bd39
kenzoutenma <pankakiril@gmail.com>
2026-06-17 12:43:22 +0300

    feat: update card view

--- src/features/anime/components/popover/card-popover.tsx
+++ src/features/anime/components/popover/card-popover.tsx
@@ -1,8 +1,7 @@
-import { Popover, Table } from '@/shared/ui';
 import renderValue from '../../utils/plain-data-details';
+import styles from './card-popover.module.scss';
 
 const AnimePopover = ({ animeData }: { animeData: AnimeDataInterface }) => {
-
   const linked: Partial<AnimeDataInterface> = {
     year: animeData.year,
     status: animeData.status,
@@ -10,27 +9,16 @@ const AnimePopover = ({ animeData }: { animeData: AnimeDataInterface }) => {
   };
 
   return (
-    <Popover>
-      <Popover.Row variant="title">
-        <h4>{animeData.title}</h4>
-      </Popover.Row>
-      <Popover.Row variant="row">
-          <p>{animeData?.description && animeData.description.slice(0, 125)}...</p>
-        <Table data-align-left>
-          {Object.entries(linked).map(([key, value]) => {
-            if (!value) return;
-            return (
-              <Table.row key={key}>
-                <Table.col>
-                  {key}:
-                </Table.col>
-                <Table.col>{renderValue(value, key)}</Table.col>
-              </Table.row>
-            );
-          })}
-        </Table>
-      </Popover.Row>
-    </Popover>
+    <div className={styles.anime_card_popover_container}>
+      <h4>{animeData.title_ua}</h4>
+      <div className={styles.anime_card_popover_tags}>
+        {Object.entries(linked).map(([key, value]) => {
+          if (!value) return;
+          return renderValue(value, key);
+        })}
+      </div>
+      {animeData?.description_main && <p>{animeData?.description_main.slice(0, 125)}...</p>}
+    </div>
   );
 };
 
