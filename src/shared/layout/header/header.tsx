d676370a1761489de8c0a8da97ae1fdf235419c6
kenzoutenma <pankakiril@gmail.com>
2026-06-17 16:49:47 +0300

    feat: header

--- src/shared/layout/header/header.tsx
+++ src/shared/layout/header/header.tsx
@@ -1,42 +1,47 @@
 'use client';
-import styles from './header.module.css';
 
 import { Button, Dropdown, ProfilePicture } from '@/shared/ui';
-import clsx from 'clsx';
+import styles from './header.module.scss';
 
 import useUserProfile from '@/features/user/hooks/useUserProfile';
-import { getAccount, paths, pathsProfile } from '@/shared/constants/headersconst';
-import { useScrollDirection } from '@/shared/hooks/useScrollDirection';
+import { getAccount, pathsProfile } from '@/shared/constants/headersconst';
 import { getTranslatedText } from '@/shared/lib';
-import { useSettingsStore } from '@/shared/state/settings-store';
 import Link from 'next/link';
 import React from 'react';
-import SearchBar from './search-bar/search-bar';
 
 export default function Header() {
   const { userStoredData } = useUserProfile();
 
-  const hideHeader = useSettingsStore((state) => state.settings?.hide_header);
-  const scrollingDown = useScrollDirection(!hideHeader);
-  const shouldHideHeader = hideHeader && scrollingDown;
-
   const AccountBlock = userStoredData?.username ? ProfileBlock : AuthBlock;
 
   return (
-    <header
-      className={clsx(styles.header_wrap, shouldHideHeader ? styles.header_hidden : '')}
-    >
-      <nav className={styles.header}>
-        <Button variant="link" as={Link} href={paths.home}>
-          {getTranslatedText(`header.app`)}
-        </Button>
-        <Button variant="link" as={Link} href={paths.list}>
-          {getTranslatedText(`paths.list`)}
-        </Button>
-        <SearchBar />
-        <div className={styles.header_account_block}>
-          <AccountBlock userStoredData={userStoredData} />
+    <header className={styles.header}>
+      <nav>
+        <div className={styles.header_left}>
+          <Button as={Link} href={'/'}>
+            {getTranslatedText(`header.app`)}
+          </Button>
+          <ul>
+            <li>
+              <Button as={Link} href={'/list'}>
+                {getTranslatedText(`paths.list`)}
+              </Button>
+            </li>
+            <li>
+              <Button as={Link} href={'/explore'}>
+                Popular
+              </Button>
+            </li>
+            <li>
+              <Button as={Link} href={'/explore'}>
+                Top
+              </Button>
+            </li>
+          </ul>
         </div>
+        <ul>
+          <AccountBlock userStoredData={userStoredData} />
+        </ul>
       </nav>
     </header>
   );
@@ -55,19 +60,25 @@ const ProfileBlock = React.memo(({ userStoredData }: { userStoredData: UserProfi
 ));
 
 const AuthBlock = () => {
-  return <>
-    <Button
-      as={Link}
-      variant="link"
-      href={getAccount.registration}
-      key="registration_button_header"
-    >
-      {getTranslatedText(`header.registration`)}
-    </Button>
-    <Button as={Link} variant="primary" href={getAccount.login} key="login_button_header">
-      {getTranslatedText(`header.login`)}
-    </Button>
-  </>
-}
+  return (
+    <>
+      <li>
+        <Button
+          as={Link}
+          variant="link"
+          href={getAccount.registration}
+          key="registration_button_header"
+        >
+          {getTranslatedText(`header.registration`)}
+        </Button>
+      </li>
+      <li>
+        <Button as={Link} variant="primary" href={getAccount.login} key="login_button_header">
+          {getTranslatedText(`header.login`)}
+        </Button>
+      </li>
+    </>
+  );
+};
 
 ProfileBlock.displayName = 'ProfileBlock';
